02523f7a25ecd3795a44713af3b64f2891855334
Matt Long <long.matt.j@gmail.com>
2019-08-19 09:05:09 -0700

    Updated to use different API call

--- next_global_entry.py
+++ next_global_entry.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-"""Check for Global Entry interview openings in your city."""
+"""
+Check for Global Entry interview openings based on cities and times provided
+in the config file
+"""
 
 import sys
 import datetime
@@ -10,28 +13,32 @@ import config
 
 def log(text):
     """Write a one-line log message."""
-    print("{dt}\t{msg}".format(
-        dt=datetime.datetime.now(),
-        msg=text))
+    print("{dt}\t{msg}".format(dt=datetime.datetime.now(),msg=text))
 
 
 if __name__ == '__main__':
-    # calculate date
-    now = datetime.datetime.now()
-    delta = datetime.timedelta(weeks=config.look_ahead_weeks)
-    future = now + delta
-    request_url = config.global_entry_query_url.format(
-        timestamp=future.strftime("%Y-%m-%d"))
-    result = requests.get(request_url).json()
-    cities = [o.get('city').lower() for o in result]
-    if config.search_string.lower() in cities:
-        client = Client(config.twilio_account, config.twilio_token)
-        message = client.messages.create(
-            to=config.to_number,
-            from_=config.twilio_from_number,
-            body="Global Entry interview opportunity in {} \
-opened up just now!".format(config.search_string))
-        log("text message sent")
-        sys.exit(0)
-    log("no news")
+    count = 0
+    for k in config.search_dict.keys():
+            try:
+                next_time = requests.get(config.global_entry_query_url\
+                                             .format(id=config.search_dict[k]['id']))\
+                                         .json()[0]['startTimestamp']
+                next_time_datetime = datetime.datetime.strptime(next_time,'%Y-%m-%dT%H:%M')
+                for j in range(0,len(config.search_dict[k]['times'])):
+                    current_time = datetime.datetime.strptime(config.\
+                                                              search_dict[k]['times'][j]\
+                                                              ,'%Y-%m-%dT%H:%M')
+                    if abs(current_time-next_time_datetime)<=datetime.timedelta(hours=2):
+                        client = Client(config.twilio_account, config.twilio_token)
+                        message = client.messages.create(
+                                     to=config.to_number,
+                                     from_=config.twilio_from_number,
+                                     body="Global Entry interview opportunity in {} \
+                                     opened up just now at {}.\
+                                     Your flight departs/lands at {}."\
+                                     .format(k,next_time_datetime,current_time))
+                        count += 1
+            except:
+                pass
+    log("Found {} possible timeslots.".format(count))
     sys.exit(1)
