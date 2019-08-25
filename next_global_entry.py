f72f72506287ffcc5c666df3e4da56e81cf010a7
Matt Long <long.matt.j@gmail.com>
2019-08-24 18:47:43 -0700

    Small updates

--- next_global_entry.py
+++ next_global_entry.py
@@ -9,15 +9,23 @@ import datetime
 import requests
 from twilio.rest import Client
 import config
-
-
-def log(text):
-    """Write a one-line log message."""
-    print("{dt}\t{msg}".format(dt=datetime.datetime.now(),msg=text))
+   
+def test_and_send(time):
+    current_time = datetime.datetime.strptime(config.\
+                                              search_dict[k]['times'][j]\
+                                              ,'%Y-%m-%dT%H:%M')
+    if abs(current_time-time)<=datetime.timedelta(hours=2):
+        client = Client(config.twilio_account, config.twilio_token)
+        message = client.messages.create(
+                     to=config.to_number,
+                     from_=config.twilio_from_number,
+                     body="Global Entry interview opportunity in {} \
+                     opened up just now at {}.\
+                     Your flight departs/lands at {}."\
+                     .format(k,time,current_time))
 
 
 if __name__ == '__main__':
-    count = 0
     for k in config.search_dict.keys():
             try:
                 next_time = requests.get(config.global_entry_query_url\
@@ -25,20 +33,13 @@ if __name__ == '__main__':
                                          .json()[0]['startTimestamp']
                 next_time_datetime = datetime.datetime.strptime(next_time,'%Y-%m-%dT%H:%M')
                 for j in range(0,len(config.search_dict[k]['times'])):
-                    current_time = datetime.datetime.strptime(config.\
-                                                              search_dict[k]['times'][j]\
-                                                              ,'%Y-%m-%dT%H:%M')
-                    if abs(current_time-next_time_datetime)<=datetime.timedelta(hours=2):
-                        client = Client(config.twilio_account, config.twilio_token)
-                        message = client.messages.create(
-                                     to=config.to_number,
-                                     from_=config.twilio_from_number,
-                                     body="Global Entry interview opportunity in {} \
-                                     opened up just now at {}.\
-                                     Your flight departs/lands at {}."\
-                                     .format(k,next_time_datetime,current_time))
-                        count += 1
+                    if k in config.exclusion_dict.keys():
+                        if config.search_dict[k]['times'][j] in config.exclusion_dict[k]['times']:
+                            continue
+                        else:
+                            test_and_send(next_time_datetime)
+                    else:
+                        test_and_send(next_time_datetime)
             except:
-                pass
-    log("Found {} possible timeslots.".format(count))
+                continue
     sys.exit(1)
